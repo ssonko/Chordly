c1a11bb9d4316cd7fe7be58ee096c084e018acc1
Alban Ssonko <albanssonko.as@gmail.com>
2026-02-03 23:23:36 -0800

    Revert "OpenAi call update"
    This reverts commit 31411ec89c199740e6e813f93d531caa46c077bb.

--- app/main.py
+++ app/main.py
@@ -1,24 +1,11 @@
-import os
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette.middleware.sessions import SessionMiddleware
-from fastapi.templating import Jinja2Templates
-
 from app.api.routes import router
 
-load_dotenv()
+app = FastAPI(title="Chordly")
 
-app = FastAPI(title="Instand Chords")
-
-app.add_middleware(
-    SessionMiddleware,
-    secret_key=os.getenv("SESSION_SECRET_KEY")
-)
-
-templates = Jinja2Templates(directory="app/templates")
-app.state.templates = templates
-
+# Static files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# Include all page + API routes
 app.include_router(router)
